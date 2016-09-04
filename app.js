a66ead479534498d7b5a975abaca266f127c6523
Trenton Kress <trenton.kress@gmail.com>
2016-09-04 14:11:04 -0700

    Index fixed and re-organized JS

--- app.js
+++ app.js
@@ -1,53 +1,45 @@
-var convApp {
+function Conversion(fromUnit, ratio) {
+  this.fromUnit = fromUnit;
+  this.ratio = ratio;
+}
+
+Conversion.prototype.convert = function(unit, number) {
+  if (unit === toUnit) {
+    return { result: number * ratio, unit: fromUnit };
+  } else if (unit === fromUnit) {
+    return { result: number / ratio, unit: toUnit };
+  } else {
+    console.log('Invalid conversion.');
+    return -1;
+  }
+};
+
+var convApp = {
   log: [],
 
-  unit: {
-    'inches',
-    'centimeters',
-    'fluid-ounces',
-    'milliliters',
-    'miles',
-    'kilometers',
-    'pounds',
-    'kilograms',
-    'shit-tons',
-    'metric-shit-tons'
-  },
-
   conversions: [
-    new Conversion(INCHES, CENTIMETERS, 2.54),
-    new Conversion(FLUID_OZ, MILLILITERS, 29.5735),
-    new Conversion(MILES, KILOMETERS, 1.60934),
-    new Conversion(POUNDS, KILOGRAMS, 0.453592),
-    new Conversion(SHIT_TON, METRIC_SHIT_TON, 24)
+    new Conversion('inches', 'centimeters', 2.54),
+    new Conversion('fluid-ounces', 'milliliters', 29.5735),
+    new Conversion('miles', 'kilometers', 1.60934),
+    new Conversion('pounds', 'kilograms', 0.453592),
+    new Conversion('shit-tons', 'metric-shit-tons', 24)
   ],
-
-  Conversion: function(fromUnit, ratio) {
-    this.fromUnit = fromUnit;
-    this.ratio = ratio;
-  },
 
   getConversion: function(fromUnit) {
     for(conversion in this.conversions) {
-      if (conversion.fromUnit === fromUnit || conversion.toUnit === fromUnit)
+      if (conversion.fromUnit == fromUnit || conversion.toUnit == fromUnit)
         return conversion;
     }
   },
 
   submitConversion: function(event) {
+    event.preventDefault();
     var toConvert = parseFloat(event.target.fromNum.value);
     var fromUnit = event.target.fromUnit.value;
-    console.log(toConvert + ' ' + fromUnit + ' is ' + getConversion(fromUnit).convert(toConvert) + ' whatevers.');
+    console.log('submitted:', toConvert, fromUnit);
+    var conversion = convApp.getConversion(fromUnit).convert(toConvert);
+    console.log(toConvert + ' ' + fromUnit + ' is ' + conversion.result + ' ' + conversion.unit);
   }
 }
 
-convApp.Conversion.prototype.convert(unit, number) {
-  if (unit === toUnit) {
-    return number * ratio;
-  } else if (unit === fromUnit) {
-    return number / ratio;
-  } else {
-    console.log('Invalid conversion.');
-    return -1;
-  }
-}
+document.getElementById('calc-form').addEventListener('submit', convApp.submitConversion);
